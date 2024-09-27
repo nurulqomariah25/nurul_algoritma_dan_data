f6a0f3d7bb1f7f1a0da8317b4fea6c362808855c
nurulqomariah25 <nurulqomariah25122005@gmail.com>
2024-09-27 10:53:29 +0700

    Add files via upload

---
+++ biodata nurul.py
@@ -0,0 +1,14 @@
+nama_depan = "nurul"
+nama_belakang ="qomariah"
+nama_lengkap = nama_depan + '' + nama_belakang
+usia ="18"
+alamat = 'semabung lama'
+kata_mutiara ="good things take time"
+
+print(nama lengkap,'(' + usia +')',',','dari',alamat,',kata mutiara:',kata_mutiara
+
+print('\nTipe dari nama_lengkap:',type(nama_lengkap))
+print('Tipe dari usia:',type(usia))
+print('Tipe dari alamat:',type(alamat))
+print('Tipe dari kata_mutiara:',type(kata_mutiara))
+
